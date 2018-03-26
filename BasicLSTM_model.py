87a3dd8f747565617cea72d584d3552e2cded6f1
AAYUSH <aayush46d@gmail.com>
2018-03-25 20:21:33 -0400

    Encoder Decoder Structure/Along with first prediction

--- BasicLSTM_model.py
+++ BasicLSTM_model.py
@@ -2,12 +2,16 @@ import pickle
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import lookup_ops
+from tensorflow.python.layers.core import Dense
+
 
 emb_size = 50
+batch_size = 5
 eos_id = 1
 
 embedings = np.loadtxt("vocab_embedings.txt");
 vocab = np.loadtxt("vocab.txt", dtype="str");
+vocab_size = vocab.size
 
 dict = {word: embeding for (word, embeding) in zip(vocab, embedings)}
 
@@ -16,6 +20,7 @@ emb_mat = tf.constant(embedings)
 vocab = lookup_ops.index_table_from_file(
   "vocab.txt", default_value= 0)
 
+reverse_vocab = tf.contrib.lookup.index_to_string_table_from_file("vocab.txt",default_value='unk')
 
 src_dataset = tf.data.TextLineDataset("text.txt")
 tgt_dataset = tf.data.TextLineDataset("summary.txt")
@@ -48,7 +53,7 @@ dataset = dataset.map(
 
 def batching_func(x):
     return x.padded_batch(
-        5, #batch size
+        batch_size, #batch size
         padded_shapes=(
             tf.TensorShape([None]),  # src
             tf.TensorShape([None]),
@@ -101,16 +106,71 @@ with tf.Session() as sess:
     sess.run(tf.tables_initializer())
     sess.run(iterator.initializer, feed_dict=None)
     res =sess.run(y)
-    print(res[0].shape, res)
-    res =sess.run(y)
-    print(res[0].shape, res)
-    res =sess.run(y)
-    print(res[0].shape, res)
-    res =sess.run(y)
-    print(res[0].shape, res)
-    res =sess.run(y)
-    print(res[0].shape, res)
+
+    inputs_index = res[0]
 
 
-    res =sess.run(encoder_emb_inp)
-    print(res)
+    embedded_inputs = tf.nn.embedding_lookup(
+          emb_mat, inputs_index)
+
+    print("chkkk --->",inputs_index.shape)
+    print("conversion", embedded_inputs.shape)
+
+    input_sequence_length = res[2]
+    print("seq shape", res[2].shape)
+    label_index = res[1]
+    embedded_labels = tf.nn.embedding_lookup(
+          emb_mat, label_index)
+    print("label indexxx------->", label_index.shape)
+    print("eeeeeeeemmmm   label indexxx------->", embedded_labels.shape)
+
+    labels_sequence_length = res[3]
+    print("out seq shape ", res[3].shape)
+
+    cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=64)
+    encoder_output, encoder_state = tf.nn.dynamic_rnn(
+        cell=cell,
+        dtype=tf.float64,
+        sequence_length=input_sequence_length,
+        inputs=embedded_inputs)
+
+    # result = tf.contrib.learn.run_n(
+    #     {"outputs": encoder_output, "last_states": encoder_state},
+    #     n=1,
+    #     feed_dict=None)
+    # print(result[0].get('outputs').shape)
+    #
+    # print(result[0].get('last_states')[0].shape)
+    #
+    # print(result[0].get('last_states')[1].shape)
+
+    # Decoder
+    decoder_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units= 64)
+
+    projection_layer = Dense(units=vocab_size, use_bias=True)
+
+    helper = tf.contrib.seq2seq.TrainingHelper(
+        embedded_labels, [max(labels_sequence_length) - 1 for _ in range(batch_size)]
+        , time_major=False)
+
+    decoder = tf.contrib.seq2seq.BasicDecoder(
+        decoder_cell, helper, encoder_state,
+        output_layer=projection_layer)
+    """Dnamic decoder"""
+    outputs, output_states, output_seq_length = tf.contrib.seq2seq.dynamic_decode(
+        decoder, output_time_major=False,
+        swap_memory=False
+    )
+
+    result = tf.contrib.learn.run_n(
+        {"outputs": outputs, "states": output_states},
+        n=1,
+        feed_dict=None)
+    print(result[0].get('outputs').rnn_output.shape)
+    x = reverse_vocab.lookup(tf.constant(result[0].get('outputs').sample_id, tf.int64))
+    print([[word.decode() for word in x] for x in sess.run(x)])
+
+    # print(result[1].size)
+
+    # res =sess.run(encoder_emb_inp)
+    # print(res)
